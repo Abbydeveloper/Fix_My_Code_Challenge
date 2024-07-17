3eb4034bdc1e612f8a427fc628d922064e955a9e
Adeboga Abigail <abbyboga@gmail.com>
2024-07-17 05:16:19 -0700

    Fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 class square():
-    
+    """Square Class"""
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """Initialize the square class"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +16,15 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string representation of the square"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
+    """Create an instance of the square class"""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
